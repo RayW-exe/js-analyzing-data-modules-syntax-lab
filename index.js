7952606ca9faa04a8f5fc38357de9ed8a4fe22fb
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-04-16 01:02:45 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const datejs = require('datejs');
 
+function combineUsers (...args){
+  let combinedObject = {
+    users: []
+  };
+  args.forEach(subArr =>{
+    combinedObject.users.push(...subArr)
+  });
+  combinedObject.merge_date = datejs.format(M/d/yyyy)
+  
+  return combinedObject
+}
 
 
 module.exports = {
